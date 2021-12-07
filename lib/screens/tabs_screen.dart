5f6c720299c875dbb2d16b6810fccb09cb90a464
nawal.bhengra@stackmybiz.com <nawal.bhengra@stackmybiz.com>
2021-12-07 18:47:35 +0530

    complete app

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,27 +1,37 @@
-import 'favorites_screen.dart';
-
-import 'categories_screen.dart';
 import 'package:flutter/material.dart';
-import './categories_screen.dart';
 
-class tabsScreen extends StatefulWidget {
+import '../widgets/main_drawer.dart';
+import './favorites_screen.dart';
+import './categories_screen.dart';
+import '../models/meal.dart';
+
+class TabsScreen extends StatefulWidget {
+  final List<Meal> favoriteMeals;
+
+  TabsScreen(this.favoriteMeals);
+
   @override
-  _tabsScreenState createState() => _tabsScreenState();
+  _TabsScreenState createState() => _TabsScreenState();
 }
 
-class _tabsScreenState extends State<tabsScreen> {
-  final List<Map<String, Object>> _pages = [
-    {
-      'page': CategoriesScreen(),
-      'title': 'Categories',
-    },
-    {
-      'page': FavoritsScreen(),
-      'title': 'Your Favorite',
-    },
-  ];
-
+class _TabsScreenState extends State<TabsScreen> {
+  List<Map<String, Object>> _pages;
   int _selectedPageIndex = 0;
+
+  @override
+  void initState() {
+    _pages = [
+      {
+        'page': CategoriesScreen(),
+        'title': 'Categories',
+      },
+      {
+        'page': FavoritesScreen(widget.favoriteMeals),
+        'title': 'Your Favorite',
+      },
+    ];
+    super.initState();
+  }
 
   void _selectPage(int index) {
     setState(() {
@@ -35,6 +45,7 @@ class _tabsScreenState extends State<tabsScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title']),
       ),
+      drawer: MainDrawer(),
       body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
@@ -42,43 +53,20 @@ class _tabsScreenState extends State<tabsScreen> {
         unselectedItemColor: Colors.white,
         selectedItemColor: Theme.of(context).accentColor,
         currentIndex: _selectedPageIndex,
-        type: BottomNavigationBarType.shifting,
+        // type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(
-              backgroundColor: Theme.of(context).primaryColor,
-              icon: Icon(Icons.category),
-              title: Text('Categories')),
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
+          ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.category), title: Text('Favorites'))
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            title: Text('Favorites'),
+          ),
         ],
       ),
     );
-
-    //!***** tab view on TOP *****!
-    // DefaultTabController(
-    //   // initialIndex: 0,
-    //   length: 2,
-    //   child: Scaffold(
-    //     appBar: AppBar(
-    //       title: Text('Meals'),
-    //       bottom: TabBar(
-    //         tabs: [
-    //           Tab(
-    //             icon: Icon(Icons.category),
-    //             text: 'Categories',
-    //           ),
-    //           Tab(
-    //             icon: Icon(Icons.star),
-    //             text: 'Favorites',
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //     body: TabBarView(children: [
-    //       CategoriesScreen(),
-    //       FavoritsScreen()
-    //     ],),
-    //   ),
-    // );
   }
 }
